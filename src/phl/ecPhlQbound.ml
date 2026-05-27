1127f2cdaf1a78e89debfaa3470f434aafd0ba87
helderm64 <heldervfmm6@gmail.com>
2026-05-27 10:00:08 +0100

    qbound tactic change

--- src/phl/ecPhlQbound.ml
+++ src/phl/ecPhlQbound.ml
@@ -7,24 +7,21 @@ open EcCoreModules
 open EcUtils
 open EcDecl
 
-(*let modtype_of_param_proc (params : (EcIdent.t * module_type) list) (xp : xpath) :  module_type option =
-  let top = EcPath.m_functor xp.EcPath.x_top in
-  match EcPath.mget_ident_opt top with
-  | None -> None
-  | Some pid ->
-      Some (List.find_map (fun (id, mty) -> if EcIdent.id_equal id pid then Some mty else None) params)
-*)
-let check (a : EcDecl.axiom) (f : xpath) (o : xpath) (env : env) : bool =
+
+let c_id = EcIdent.create "c"
+let c    = f_local c_id EcTypes.tint   
+
+let check (a : EcDecl.axiom) (callee : xpath) (f : xpath) (o : xpath) (env : env) : bool =
   let form = a.ax_spec in
   match form.f_node with 
   | Fqbound qb -> 
-    Format.eprintf "f' = %s\n%!" (EcFol.dump_f form);
+    (*Format.eprintf "f'1 = %s\n%!" (EcFol.dump_f form);*)
     let s = f_bind_mod (f_subst_init ()) (mget_ident qb.qb_orcl.x_top) f.x_top env in
     let f' = EcCoreSubst.Fsubst.f_subst s form in
-    Format.eprintf "f' = %s\n%!" (EcFol.dump_f f');
+    (*Format.eprintf "f'2 = %s\n%!" (EcFol.dump_f f');*)
     begin
     match f'.f_node with     
-      |Fqbound qb -> (*qmod_or_proc_equal qb.qb_proc (Qproc f) &&*) x_equal qb.qb_orcl o
+      |Fqbound qb -> (*qmod_or_proc_equal qb.qb_proc (Qproc callee) &&*) x_equal qb.qb_orcl o
       | _ -> assert false
     end
   | _ -> false
@@ -32,29 +29,53 @@ let check (a : EcDecl.axiom) (f : xpath) (o : xpath) (env : env) : bool =
 let sum_int_forms (fs : form list) : form =
   List.fold_left f_int_add_simpl f_i0 fs
 
-let rec qbound (fb : stmt) (o : xpath) (env : env) =
+let rec qbound_concrete (fb : stmt) (o : xpath) (env : env) =
   let is = fb.s_node in 
   let doit i =
     match i.i_node with
     | Scall (_, x, _) -> 
        let f' = Fun.by_xpath x env in
        begin
+       let cmag = if f'.f_quantum = `Quantum then c else f_i1 in
        if x_equal o x then f_i1
-       else 
+       else
         match f'.f_def with
-        | FBdef fdef -> qbound fdef.f_body o env 
-        | FBabs oi -> 
-          let oi = List.map (NormMp.norm_xfun env) oi.oi_calls in
-          Format.eprintf "length : %s\n%!" (string_of_int (List.length oi));
-          List.iter (fun (_,a) -> Format.eprintf "axioms: %s@." (EcFol.dump_f a.ax_spec)) (Ax.all ~check:(fun _ ax -> match ax.ax_loca with `Local | `Declare -> true | _ -> false) env);
-          let l = List.snd (Ax.all ~check:(fun _ a -> List.exists (fun o' -> check a o o' env) oi) env) in
-          let qbl = List.map (fun a -> match a.ax_spec.f_node with Fqbound qb -> qb.qb_bound | _ -> assert false) l in
-          sum_int_forms qbl
+        | FBdef fdef -> f_int_mul_simpl cmag (qbound_concrete fdef.f_body o env)
+        | FBabs oi -> (*List.iter (fun x -> Format.eprintf "oracle : %s\n%!" (EcPath.x_tostring x)) oi.oi_calls;*)
+                      f_int_mul_simpl cmag (qbound_abstract x oi o env)
         | _ -> assert false
         end 
     | _ -> f_i0
   in
   sum_int_forms (List.map doit is)
+
+and qbound_abstract (callee : xpath) (ois : oracle_info) (o : xpath) (env : env) =
+  let ois = List.map (NormMp.norm_xfun env) ois.oi_calls in
+  (*List.iter (fun x -> Format.eprintf "oracle : %s\n%!" (EcPath.x_tostring x)) ois;*)
+  if not (List.exists (fun o' -> x_equal o o') ois) then f_i0
+  else
+  let doit oi =
+    let f = Fun.by_xpath oi env in
+    (*Format.eprintf "quantum : %a\n%!" EcPrinting.pp_quantum f.f_quantum;*)
+    let cmag = if f.f_quantum = `Quantum then c else f_i1 in
+    (*Format.eprintf "cmag: %s\n%!" (EcFol.dump_f cmag);*)
+    let qo =
+    match f.f_def with
+    | FBdef fdef -> f_int_mul_simpl cmag (qbound_concrete fdef.f_body o env)
+    | FBabs ois -> f_int_mul_simpl cmag (qbound_abstract oi ois o env)
+    | _ -> assert false
+    in
+    let l = List.snd (Ax.all ~check:(fun _ a -> List.exists (fun o' -> check a callee o' oi env && not (x_equal o o')) ois) env) in
+    if List.is_empty l then f_i0
+    else
+    match (List.hd l).ax_spec.f_node with Fqbound qb -> f_int_mul qb.qb_bound qo | _ -> assert false
+  in
+  let l = List.snd (Ax.all ~check:(fun _ a -> List.exists (fun o' -> check a callee o' o env) ois) env) in
+  if List.is_empty l then f_i0
+  else
+  let qb = match (List.hd l).ax_spec.f_node with Fqbound qb -> qb .qb_bound| _ -> assert false in
+  sum_int_forms (List.map doit ois) |> f_int_add_simpl qb
+
 
 let process_qbound (tc : tcenv1) =
   let q = FApi.tc1_goal tc in
@@ -66,5 +87,5 @@ let process_qbound (tc : tcenv1) =
     | Qproc p -> p
     | Qmod _ -> assert false in
   let f = Fun.by_xpath x env in
-  let sum = match f.f_def with | FBdef fb -> qbound fb.f_body o env | _ -> assert false in
+  let sum = match f.f_def with | FBdef fb -> qbound_concrete fb.f_body o env | _ -> assert false in
   FApi.xmutate1 tc `Qbound [f_int_le qb sum]
